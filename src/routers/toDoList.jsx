add4248233ebc212b6457805bf1edfe1ac1d6462
Fujita <24aw0133@jec.ac.jp>
2025-02-10 00:11:05 +0900

    4

--- src/routers/toDoList.jsx
+++ src/routers/toDoList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import "../css/toDoList.css";
+import "../css/todoList.css";
 
 import { AiOutlineDelete } from "react-icons/ai";
 import { BsCheckLg } from "react-icons/bs";
